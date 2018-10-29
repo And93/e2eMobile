99f9437eae72aefaf77cfcf7b7f65036fe024fae
Andrei Dzeichyk <andreidei4ik@gmail.com>
2018-10-29 22:45:50 +0300

    UPD:
    - added killer for the run emulator.

---
+++ killer.bat
@@ -0,0 +1,19 @@
+tasklist /nh /fi "imagename eq qemu-system*" | find /i "qemu-system" && (
+taskkill /f /im qemu-system*
+) || (
+echo Process 'qemu-system' is not running
+)
+
+tasklist /nh /fi "imagename eq emulator*" | find /i "emulator" && (
+taskkill /f /im emulator*
+) || (
+echo Emulator is not running
+)
+
+tasklist /nh /fi "imagename eq adb*" | find /i "adb" && (
+adb kill-server
+) || (
+echo Adb-server is not running
+)
+
+exit 0
